58198026da1eff8176881088c3f52c73d505ad0d
doragon_ <lung.trabajos@gmail.com>
2020-10-13 18:10:54 -0300

    minor details

--- src/main/java/com/github/doragonUni/finalreality/model/character/AbstractCharacter.java
+++ src/main/java/com/github/doragonUni/finalreality/model/character/AbstractCharacter.java
@@ -81,6 +81,7 @@ public abstract class AbstractCharacter implements ICharacter {
   /**
    * Adds this character to the turns queue.
    */
+  @Override
   public void addToQueue() {
     turnsQueue.add(this);
     scheduledExecutor.shutdown();
--- src/main/java/com/github/doragonUni/finalreality/model/weapon/IWeapon.java
+++ src/main/java/com/github/doragonUni/finalreality/model/weapon/IWeapon.java
@@ -2,10 +2,17 @@ package com.github.doragonUni.finalreality.model.weapon;
 
 public interface IWeapon {
 
+    /**
+     * get the Name for the Weapon
+     */
     String getName();
-
+    /**
+     * get the Damage for the Weapon
+     */
     int getDamage();
-
+    /**
+     * get the Weight for the Weapon
+     */
     int getWeight();
 
 
